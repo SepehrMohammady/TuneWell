5442e50bd6e714294a2578a7c3af3b9da8f98154
Sepehr Mohammady <SMohammady@outlook.com>
2025-12-05 12:50:33 +0100

    v0.1.1: fix icon consistency

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -20,8 +20,8 @@ const ROOT_DIR = path.resolve(__dirname, '..');
 const VERSION = {
   major: 0,
   minor: 1,
-  patch: 0,
-  build: 22,
+  patch: 1,
+  build: 23,
 };
 
 const versionString = `${VERSION.major}.${VERSION.minor}.${VERSION.patch}`;
